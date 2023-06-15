dbe6c824924c704e5bd29c881a8bff25af0401f6
kamo16-cmd <kgomotsotlhasi16@gmail.com>
2023-06-15 06:24:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - the entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
-}
-		
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
