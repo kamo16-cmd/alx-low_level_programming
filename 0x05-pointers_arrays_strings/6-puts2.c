401b0525e7a93bf6512721cbc8d0b23ca1c3e37f
kamo16-cmd <kgomotsotlhasi16@gmail.com>
2023-06-26 12:40:04 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,18 @@
-#include <stdio.h>
+#include "holberton.h"
 
-void puts2(const char *str) {
-    int i;
+/**
+ * puts2 - prints every other character starting with the first
+ * character, followed by a new line.
+ * @str: the string to manipulate.
+ *
+ * Return: nothing.
+ */
+void puts2(char *str)
+{
+	int i;
 
-    for (i = 0; str[i] != '\0'; i += 2) {
-        printf("%c", str[i]);
-    }
-
-    printf("\n");
+	for (i = 0; str[i] != '\0'; i++)
+		if (!(i % 2))
+			_putchar(str[i]);
+	_putchar(10);
 }
-
-int main() {
-    const char *str = "Hello, World!";
-    puts2(str);
-
-    return 0;
-}
-
