f3fd64c3b7b671215f1d250d4da37bd22f9a0ba9
kamo16-cmd <kgomotsotlhasi16@gmail.com>
2023-08-01 16:56:42 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * free_listint2 - frees the linked list
- * @head : pointer to the listint_t list to be freed 
+ * @head : pointer to the listint_t list to be freed
  */
 void free_listint2(listint_t **head)
 {
